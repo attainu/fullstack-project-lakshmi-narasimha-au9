63e485a11317a2ce3fa7fa3097bbdb4c6c40f192
lucky <lmnarasimha2301@gmail.com.com>
2021-03-11 09:07:07 +0530

    project initialised - User model created

---
+++ controller/authRouter.js
@@ -0,0 +1,53 @@
+const Router = require('express')();
+const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+const Joi = require('@hapi/joi')        // for requests validation
+require('dotenv/config');
+
+
+// validator Schema
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email:Joi.string().min(6).required().email(),
+    password:Joi.string().min(6).required(),
+    dob:Joi.string().required().isoDate()
+})
+
+
+
+// register users
+Router.post('/register', async(req,res)=>{
+    // validate req.body
+    const {error} = schema.validate(req.body)
+    if(error) return res.status(400).send(error)
+    
+    const hashed_password = await bcrypt.hashSync(req.body.password, 9);
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashed_password,
+        DOB: req.body.dob
+    })
+    try{
+        await user.save()
+        res.status(200).send("registered successfully")
+    }catch(err){
+        res.status(500).send(err)
+    }
+    
+})
+
+// Get all users
+
+// Update user
+
+// Deactivate user
+
+// Delete user
+
+// Authenticate/login Users
+
+
+
+
+module.exports = Router;
---
+++ models/UserModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type:String,
+        min: 6,
+        max: 20,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        min: 8,
+        max: 255,
+        required:true
+    },
+    password:{
+        type: String,
+        required:true,
+        max:1024
+    },
+    isActive:{
+        type:Boolean,
+        default: true
+    },
+    profile_img_url:{
+        type:String,
+        default:"https://drive.google.com/file/d/1q1TZddIK9ZUkjQsjhT6S8kYvadanHnhr/view?usp=sharing"
+    },
+    DOB:{
+        type: Date,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Users', UserSchema)
