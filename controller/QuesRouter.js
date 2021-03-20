af9d5e404fb99452d7a839d89891d54aa40cb966
lucky <lmnarasimha2301@gmail.com.com>
2021-03-20 12:39:22 +0530

    Ques Router completed

--- controller/QuesRouter.js
+++ controller/QuesRouter.js
@@ -21,12 +21,12 @@ Router.post('/new', (req,res)=>{
                     question: req.body.question
                 })
                 try{
-                    await question.save((err)=>{
+                    await question.save();
                         if(err) return err;
                         res.status(200).send({message:"question published successfully"});
-                    })
+                    
                 }catch(err){
-                    res.status(501).send({error:"server error, please try again later"})
+                    res.status(501).send({error:err._message})
                 }
 
             }
@@ -41,7 +41,7 @@ Router.post('/new', (req,res)=>{
 })
 
 // get questions of respective user 
-Router.get('/',(req,res)=>{
+Router.get('/user',(req,res)=>{
     if(req.headers.cookie){
         const token = req.headers.cookie.split("x-access-token=")[1];
         jwt.verify(token, process.env.JWTSECRET, async(err, data)=>{
@@ -58,12 +58,10 @@ Router.get('/',(req,res)=>{
                     res.send(err);
 
                 }
-                
             }
             catch(error){
 
             }
-            
         })
     }
     else{
@@ -74,18 +72,92 @@ Router.get('/',(req,res)=>{
 
 
 // search questions
-Router.get("/search", (req, res)=>{
+Router.get("/ques", async(req, res)=>{
     if(req.query){
-        const q = req.query;
-        res.send(q)
-    }else{
+        const q = new RegExp(req.query.search, 'i');
+        const start = req.query.start?Number(req.query.start):0
+        try{
+            await Question.find({question:q},{skip:start},{limit:10},(err,data)=>{
+            if(err) return err
+            res.send(data)
+        })}
+        catch(error){
+            res.send({error})
+        }
+    }
+    else{
         res.status(403).send({error:"search query missing"})
     }
 })
 
 // update question
+Router.put('/update/:id', async(req,res)=>{
+    if(req.headers.cookie){
+        const token = req.headers.cookie.split('x-access-token=')[1]
+        const QId = req.params.id;
+        await jwt.verify(token, process.env.JWTSECRET,async(err, data)=>{
+            if (err) return err;
+            let userId = data.id;
+            await Question.findById(QId, async(error,ques)=>{
+                if (error) return error;
+                if(ques.questioner==userId){
+                    const updatedQues = {
+                        category:req.body.category,
+                        question: req.body.question
+                    }
+                    try{
+                        await Question.updateOne({_id:QId},updatedQues,{runValidators:true},(er)=>{
+                            if (er) return er;
+                            res.status(200).send({success: `question updated successfully`})
+                        })
+                    }catch(error){
+                        res.send({error:error.message})
+                    }
+                    
+                }
+                else{
+                    res.send({error: "You're not authorised to edit this question"})
+                }
+            })
+        })
+    }
+    else{
+        res.send({error:"Login required"});
+    }
+})
+
 
 // delete question
+Router.delete('/delete/:id', async(req,res)=>{
+    if(req.headers.cookie){
+        const token = req.headers.cookie.split('x-access-token=')[1];
+        jwt.verify(token, process.env.JWTSECRET, async(err,data)=>{
+            if(err) return err;
+            let userId = data.id;
+            const Qid = req.params.id; 
+            try{
+                await Question.findById(Qid, async(err, ques)=>{
+                    if(err) return err;
+                    if(ques.questioner==userId){
+                        await Question.deleteOne({_id:Qid},(err,msg)=>{
+                            if(err) res.send({error: err.message});
+                            res.send({success:"question deleted sucessfully"})
+                        })
+                    }
+                    else{
+                        res.send({error: "You're not authorised to delete this question"})
+                    }
+                })
+            }
+            catch(error){
+                res.send({error})
+            }
+        })
+    }
+    else{
+        res.send({message:"Login required"})
+    }
+})
 
 
 module.exports = Router;
